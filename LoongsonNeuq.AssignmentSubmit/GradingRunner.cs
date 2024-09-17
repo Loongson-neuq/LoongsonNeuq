d0c0318ffa777202b0d420fb6a3a40ff487616cf
Caiyi Shyu <cai1hsu@outlook.com>
2024-09-17 20:45:18 +0800

    refactor: Add GradingRunner class for running assignment grading steps

---
+++ LoongsonNeuq.AssignmentSubmit/GradingRunner.cs
@@ -0,0 +1,67 @@
+using LoongsonNeuq.AssignmentSubmit.Configuration;
+using LoongsonNeuq.AssignmentSubmit.Models;
+using LoongsonNeuq.AutoGrader;
+using Microsoft.Extensions.Logging;
+
+namespace LoongsonNeuq.AssignmentSubmit;
+
+public class GradingRunner
+{
+    private AssignmentConfig _assignmentConfig;
+    private ILogger _logger;
+
+    private GradingConfig GradingConfig => _assignmentConfig.AutoGrade;
+
+    public GradingRunner(AssignmentConfig assignmentConfig, ILogger logger)
+    {
+        _assignmentConfig = assignmentConfig;
+        _logger = logger;
+    }
+
+    public List<StepPayload?>? Run()
+    {
+        if (!GradingConfig.Enabled || GradingConfig.Steps is null || GradingConfig.Steps.Count == 0)
+        {
+            return null;
+        }
+
+        List<StepPayload?> stepResults = new();
+
+        foreach (var step in GradingConfig.Steps)
+        {
+            var stepRunner = new StepRunner(step);
+
+            StepResult? result = null;
+
+            try
+            {
+                result = stepRunner.RunAsync().Result;
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning($"Failed to run step: {step.Title}, No score will be given. Message: {e.Message}");
+            }
+
+            if (result is not null)
+            {
+                stepResults.Add(new StepPayload
+                {
+                    Title = step.Title,
+                    ElapsedSeconds = result.ElapsedSeconds,
+                    PeakWorkingSet64 = result.PeakWorkingSet64,
+                    ExitCode = result.ExitCode,
+                    Score = result.Score,
+                    Failed = result.Failed,
+                    ReachedTimeout = result.ReachedTimeout
+                });
+            }
+            else
+            {
+                // placeholder for failed step
+                stepResults.Add(null);
+            }
+        }
+
+        return stepResults;
+    }
+}
