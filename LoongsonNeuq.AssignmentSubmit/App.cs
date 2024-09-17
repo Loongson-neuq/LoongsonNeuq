32d6b715d59f97b28f35ce8de247f955b7e71208
Caiyi Shyu <cai1hsu@outlook.com>
2024-09-17 20:45:44 +0800

    Implement assignment submit main logic

--- LoongsonNeuq.AssignmentSubmit/App.cs
+++ LoongsonNeuq.AssignmentSubmit/App.cs
@@ -1,5 +1,13 @@
 using Microsoft.Extensions.Logging;
 using LoongsonNeuq.AssignmentSubmit.Configuration;
+using LoongsonNeuq.AssignmentSubmit.Models;
+using LoongsonNeuq.Common.Environments;
+using GitHub;
+using GitHub.Repos.Item.Item.Dispatches;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+using Microsoft.Kiota.Abstractions.Serialization;
+using LoongsonNeuq.AssignmentSubmit.Submitters;
 
 namespace LoongsonNeuq.AssignmentSubmit;
 
@@ -8,21 +16,92 @@ public class App
     private readonly ILogger _logger;
     private readonly AssignmentConfig _config;
 
-    public App(ILogger logger, AssignmentConfig config)
+    private readonly GitHubActions _gitHubActions;
+    private readonly GitHubClient _gitHubClient;
+
+    private readonly GradingRunner _gradingRunner;
+    private readonly ResultSubmitter _resultSubmitter;
+    private SubmitPayload submitPayload = new SubmitPayload();
+
+    public App(ILogger logger, AssignmentConfig config, GitHubActions gitHubActions, GitHubClient gitHubClient, GradingRunner gradingRunner, ResultSubmitter resultSubmitter)
     {
         _logger = logger;
-        _config = config;        
+        _config = config;
+        _gitHubActions = gitHubActions;
+        _gitHubClient = gitHubClient;
+        _gradingRunner = gradingRunner;
+        _resultSubmitter = resultSubmitter;
+    }
+
+    private ExitCode fillSubmitPayload()
+    {
+        submitPayload.GitHubId = _gitHubActions.Actor!;
+        submitPayload.AssignmentId = _config.CategorySpecificAssignmentId;
+        submitPayload.Timestamp = DateTimeOffset.Now.ToUnixTimeSeconds();
+        submitPayload.AssignmentRepo = _gitHubActions.Repository!;
+        submitPayload.RepoSha = _gitHubActions.Sha!;
+
+        _logger.LogInformation("Submit payload filled");
+        _logger.LogInformation($"    GitHubId       : {submitPayload.GitHubId}");
+        _logger.LogInformation($"    AssignmentId   : {submitPayload.AssignmentId}");
+        _logger.LogInformation($"    Timestamp      : {submitPayload.Timestamp}");
+        _logger.LogInformation($"    AssignmentRepo : {submitPayload.AssignmentRepo}");
+        _logger.LogInformation($"    RepoSha        : {submitPayload.RepoSha}");
+
+        if (string.IsNullOrEmpty(submitPayload.GitHubId))
+        {
+            _logger.LogError("Failed to get GitHubId");
+            return ExitCode.FailedToGetGitHubId;
+        }
+
+        if (string.IsNullOrEmpty(submitPayload.AssignmentRepo))
+        {
+            _logger.LogError("Failed to get AssignmentRepo");
+            return ExitCode.FailedToGetAssignmentRepo;
+        }
+
+        if (string.IsNullOrEmpty(submitPayload.RepoSha))
+        {
+            _logger.LogError("Failed to get RepoSha");
+            return ExitCode.FailedToGetRepoSha;
+        }
+
+        return ExitCode.Success;
     }
 
     public ExitCode Run()
     {
-        if (_config is null)
+        _logger.LogInformation("AssignmentSubmit started");
+
+        if (!_gitHubActions.IsCI)
         {
-            _logger.LogError("Config was not loaded, exiting");
-            return ExitCode.ConfigError;
+            _logger.LogError("Not running in CI, exiting");
+            return ExitCode.NotInCI;
         }
 
-        // TODO: Implement the logic here
+        var fill = fillSubmitPayload();
+
+        if (fill != ExitCode.Success)
+        {
+            _logger.LogError("Failed to fill submit payload, exiting");
+            return fill;
+        }
+
+        if (_config.AutoGrade.Enabled)
+        {
+            _logger.LogInformation("Auto grading enabled, Starting auto grading");
+
+            submitPayload.StepResults = _gradingRunner.Run();
+        }
+        else
+        {
+            _logger.LogInformation("Auto grading disabled, Skipping auto grading");
+        }
+
+        _resultSubmitter.AssignmentConfig = _config;
+        _resultSubmitter.SubmitPayload = submitPayload;
+
+        _resultSubmitter.SubmitResult();;
 
         return ExitCode.Success;
     }
--- LoongsonNeuq.AssignmentSubmit/ExitCode.cs
+++ LoongsonNeuq.AssignmentSubmit/ExitCode.cs
@@ -4,4 +4,8 @@ public enum ExitCode : int
 {
     Success = 0,
     ConfigError = 1,
+    NotInCI = 2,
+    FailedToGetGitHubId = 3,
+    FailedToGetAssignmentRepo = 4,
+    FailedToGetRepoSha = 5,
 }
