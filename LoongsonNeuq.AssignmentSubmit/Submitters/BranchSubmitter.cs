ff032691cd61d0d6e0cf571d497282d0797d1c20
Caiyi Shyu <cai1hsu@outlook.com>
2024-09-23 21:20:42 +0800

    improve `StdOut` and `StdErr` title

--- LoongsonNeuq.AssignmentSubmit/Submitters/BranchSubmitter.cs
+++ LoongsonNeuq.AssignmentSubmit/Submitters/BranchSubmitter.cs
@@ -134,7 +134,7 @@ public class BranchSubmitter : ResultSubmitter
         if (SubmitPayload.StepPayloads is null)
             return;
 
-        docBuilder.AppendLine("| Step | Score | Elapsed | Peak Memory | LTE | Pass | Stdout | Stderr |");
+        docBuilder.AppendLine("| Step | Score | Elapsed | Peak Memory | LTE | Pass | StdOut | StdErr |");
         docBuilder.AppendLine("|------|-------|---------|-------------|-----|------|--------|--------|");
 
         foreach (var step in SubmitPayload.StepPayloads)
